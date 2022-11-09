e4086eb353ed788d879f7b7a1d0698f96cf74afd
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-11-10 01:11:05 +0900

    20221110-1

---
+++ 3DPortfolioPractice/Assets/05. Scripts/Weapon/GunAmmoPickUp.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GunAmmoPickUp : MonoBehaviour
+{
+    public GameObject fakeAmmoClip;
+    public AudioSource ammoPickupSound;
+    public GameObject pickUpDisplay; // 텍스트 관련 게임오브젝트 변수명 선언
+
+    private void OnTriggerEnter(Collider other) // Cube의 콜라이더에 닿을 시 발동되는 메서드
+    {
+        fakeAmmoClip.SetActive(false); // 가짜 탄약 팩 비활성화
+        ammoPickupSound.Play(); // 탄약 팩을 들어올릴 시에 들리는 소리 활성화
+        GlobalAmmo.gunAmmo += 10; // 탄약 팩 안에 든 총알 수
+        pickUpDisplay.SetActive(false); // 텍스트 게임오브젝트 비활성화
+        pickUpDisplay.GetComponent<TextMeshProUGUI>().text = "CLIP OF BULLETS"; // 텍스트 메쉬프로 컴포넌트를 가져와 "CLIP OF BULLETS" 나타내도록 설정
+        pickUpDisplay.SetActive(true); // 텍스트 게임 오브젝트 활성화
+    }
+}
---
+++ 3DPortfolioPractice/Assets/05. Scripts/Weapon/HandgunFire.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunFire : MonoBehaviour
+{
+    public GameObject theGun; // 총 오브젝트
+    public GameObject muzzleFlash; // 플레시 오브젝트
+    public AudioSource gunFire; // 총 쏠 때 재생되는 효과음
+    public bool isFiring = false; // 총이 발사되지 않도록 false값으로 초기화
+    public AudioSource emptySound;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) // Fire1 키를 누르면
+        {
+            if(GlobalAmmo.gunAmmo < 1)
+            {
+                emptySound.Play();
+            }
+            else
+            {
+                if (isFiring == false)
+                {
+                    StartCoroutine(FiringHandgun()); // 총올 쏠 때 발동되는 메서드
+                }
+            } 
+        }
+    }
+
+    IEnumerator FiringHandgun()
+    {
+        isFiring = true; // 총이 발사될 수 있도록 bool값을 true로 초기화
+        GlobalAmmo.gunAmmo -= 1; // 발사 시 줄어드는 탄환수
+        theGun.GetComponent<Animator>().Play("GunFire"); // 총의 Animator 컴포넌트의 GunFire 이름의 애니메이션 발동
+        muzzleFlash.SetActive(true); // 총이 발사될 때 보이는 빛 오브젝트 활성화
+        gunFire.Play(); // 총알이 발사될 때 재생되는 효과음
+        yield return new WaitForSeconds(0.03f); // 0.03초 동안 대기
+        muzzleFlash.SetActive(false); // 빛 오브젝트 비활성화
+        yield return new WaitForSeconds(0.25f); // 0.25초 동안 대기
+        isFiring = false; // 총이 발사되지 않도록 bool 값에 false 할당
+    }
+}
